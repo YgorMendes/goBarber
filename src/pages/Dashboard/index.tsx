f9e3a76a9516653583c14e0d84438de73e7fc85c
Ygor Mendes <ygortmendes@gmail.com>
2021-03-02 14:45:12 -0300

    list appointment

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import { FiPower } from 'react-icons/fi';
+import { FiClock, FiPower } from 'react-icons/fi';
 import { useAuth } from '../../hooks/auth';
-import { Container, Header, Profile } from './styles';
+import {
+  Container,
+  Header,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Calendar,
+  Section,
+  Appointment,
+} from './styles';
 import Logo from '../../assets/icons/logo.svg';
 
 function Home(): JSX.Element {
   const { signOut, user } = useAuth();
+  const [selectDate, setSelectDate] = React.useState(new Date());
 
   const handleClick = React.useCallback(() => {
     signOut();
@@ -17,7 +28,10 @@ function Home(): JSX.Element {
         <img src={Logo} alt="logo" />
 
         <Profile>
-          <img src={user.avatar_url} alt="description" />
+          <img
+            src="https://scontent.fcgh5-1.fna.fbcdn.net/v/t31.0-8/18077219_284318775359631_700044029426251945_o.jpg?_nc_cat=109&ccb=3&_nc_sid=730e14&_nc_eui2=AeEh3t8UF5iWkKVgTsCjwh3FPlzeQ796hVg-XN5Dv3qFWET5SgM_5ah9YBBCuuAz3P4TCOs4yAcceGxH8vNapq5o&_nc_ohc=VHau98i7MpwAX_pSlFz&_nc_ht=scontent.fcgh5-1.fna&oh=6d00e7abfba43996d81f91a4e647eff6&oe=6065A71C"
+            alt="description"
+          />
           <div>
             <p>wellcome!</p>
             <strong>{user.name}</strong>
@@ -28,6 +42,78 @@ function Home(): JSX.Element {
           <FiPower />
         </button>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>appointments hours</h1>
+          <p>
+            <span>today | </span>
+            <span>day 02 | </span>
+            <span>Monday</span>
+          </p>
+
+          <NextAppointment>
+            <strong>appointment to follow</strong>
+            <div>
+              <img
+                src="https://s3.envato.com/files/297753474/IMG_7074.jpg"
+                alt="barber woman"
+              />
+              <strong>Barber woman</strong>
+              <span>
+                <FiClock />
+                13:00
+              </span>
+            </div>
+          </NextAppointment>
+          <Section>
+            <strong>morning</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                13:00
+              </span>
+              <div>
+                <img
+                  src="https://s3.envato.com/files/297753474/IMG_7074.jpg"
+                  alt="barber woman"
+                />
+                <strong>Barber woman</strong>
+              </div>
+            </Appointment>
+            <Appointment>
+              <span>
+                <FiClock />
+                13:00
+              </span>
+              <div>
+                <img
+                  src="https://s3.envato.com/files/297753474/IMG_7074.jpg"
+                  alt="barber woman"
+                />
+                <strong>Barber woman</strong>
+              </div>
+            </Appointment>
+          </Section>
+          <Section>
+            <strong>afternoon</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                13:00
+              </span>
+              <div>
+                <img
+                  src="https://s3.envato.com/files/297753474/IMG_7074.jpg"
+                  alt="barber woman"
+                />
+                <strong>Barber woman</strong>
+              </div>
+            </Appointment>
+          </Section>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 }
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -2,10 +2,10 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
-  background-color: #28262e;
 `;
 
 export const Header = styled.header`
+  background-color: #28262e;
   padding: 20px 12%;
 
   display: flex;
@@ -34,11 +34,139 @@ export const Profile = styled.div`
   align-items: center;
   margin-left: 80px;
 
+  img {
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+  }
+
   div {
     margin-left: 12px;
   }
 
   strong {
     color: #f8902c;
+  }
+`;
+
+export const Content = styled.main`
+  margin: 64px auto;
+  display: flex;
+  padding: 0 12%;
+`;
+export const Schedule = styled.div`
+  flex: 1;
+  margin-right: 120px;
+
+  h1 {
+    font-size: 42px;
+  }
+
+  p {
+    margin-top: 20px;
+  }
+
+  p > span {
+    color: #f8902c;
+  }
+`;
+export const Calendar = styled.aside`
+  width: 380px;
+`;
+
+export const NextAppointment = styled.div`
+  margin-top: 64px;
+
+  > strong {
+    color: #999591;
+  }
+
+  div {
+    width: 520px;
+    background-color: #3e3b47;
+    margin-top: 24px;
+    display: flex;
+    align-items: center;
+    padding: 16px;
+    border-radius: 8px;
+
+    img {
+      width: 60px;
+      height: 60px;
+      border-radius: 50%;
+    }
+
+    strong {
+      margin-left: 20px;
+      font-size: 28px;
+    }
+
+    span {
+      display: flex;
+      align-items: center;
+      margin-left: auto;
+
+      svg {
+        color: #f8902c;
+        margin-right: 8px;
+      }
+    }
+  }
+`;
+
+export const Section = styled.div`
+  margin-top: 48px;
+
+  > strong {
+    color: #999591;
+    font-size: 20px;
+    line-height: 26px;
+    border-bottom: 1px solid #3e3b47;
+    display: block;
+    padding-bottom: 16px;
+    margin-bottom: 16px;
+  }
+`;
+
+export const Appointment = styled.div`
+  display: flex;
+  align-items: center;
+
+  & + div {
+    margin-top: 16px;
+  }
+
+  span {
+    margin-left: auto;
+    display: flex;
+    align-items: center;
+    color: #f4ede8;
+
+    svg {
+      color: #f8902c;
+      margin-right: 8px;
+    }
+  }
+
+  div {
+    background-color: #3e3b47;
+    display: flex;
+    flex: 1;
+    align-items: center;
+    padding: 16px 24px;
+    border-radius: 8px;
+    margin-left: 24px;
+
+    img {
+      width: 56px;
+      height: 56px;
+      border-radius: 50%;
+    }
+
+    strong {
+      margin-left: 24px;
+      color: fff;
+      font-size: 20px;
+    }
   }
 `;
