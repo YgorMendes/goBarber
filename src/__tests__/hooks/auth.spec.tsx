91acc36935d37984bd04960db6ba9fa356f39eb1
Ygor Mendes <ygortmendes@gmail.com>
2021-03-08 15:24:13 -0300

    mock api

--- src/__tests__/hooks/auth.spec.tsx
+++ src/__tests__/hooks/auth.spec.tsx
@@ -1,17 +1,35 @@
 import { renderHook } from '@testing-library/react-hooks';
+import MockAdapter from 'axios-mock-adapter';
 import { useAuth, AuthProvider } from '../../hooks/auth';
+import api from '../../services/api';
+
+const apiMock = new MockAdapter(api);
 
 describe('Auth hook', () => {
-  it('Shoud be able to sign in', () => {
-    const { result } = renderHook(() => useAuth(), {
+  it('Shoud be able to sign in', async () => {
+    apiMock.onPost('sessions').reply(200, {
+      user: {
+        id: 'user123',
+        name: 'lala',
+        email: 'lala@lala.lala',
+      },
+      toke: 'token123',
+    });
+
+    const { result, waitForNextUpdate } = renderHook(() => useAuth(), {
       wrapper: AuthProvider,
     });
+
+    const setItens = jest.spyOn(Storage.prototype, 'setItem');
 
     result.current.signIn({
       email: 'lala@lala.lala',
       password: '123456',
     });
 
+    await waitForNextUpdate();
+
+    expect(setItens).toHaveBeenCalledTimes(2);
     expect(result.current.user.email).toEqual('lala@lala.lala');
   });
 });
